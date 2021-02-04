e7acb70d3e0aff9596f4dc7c2f027cb85e5c20fe
digitalocean-stackbit <77975946+digitalocean-stackbit@users.noreply.github.com>
2021-02-04 11:37:08 +0200

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -9,6 +9,6 @@ echo "stackbit-build.sh: start build"
 # build site
 hugo
 
-./inject-stackbit-widget.js public https://localhost:8092/init.js 601b140d004541e7842d506b
+# ./inject-stackbit-widget.js public https://localhost:8092/init.js 601b140d004541e7842d506b
 
 echo "stackbit-build.sh: finished build"
